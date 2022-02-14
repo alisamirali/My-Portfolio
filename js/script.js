1ae78cc0ead50694426faa9b8fed1f1d65800030
alisamirali <ali01114181442@gmail.com>
2022-02-15 00:20:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.typing', {
+    strings: [
+        "",
+        "Front-End Dev",
+        "Back-End Dev",
+        "Web Developer",
+        "Software Dev"
+    ],
+    typeSpeed: 120,
+    backSpeed: 60,
+    loop: true,
+});
